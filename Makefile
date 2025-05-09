7a8e939bf6ffdebadfbe806c135c4b63712fcd37
Dasdron15 <ogurechik760@gmail.com>
2025-05-09 22:57:57 +0200

    Add files

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	gcc -o brainf main.c
---
+++ main.c
@@ -0,0 +1,68 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+#define LENGTH 30000
+
+uint8_t turing[LENGTH] = {0};
+
+int main(int argc, char* argv[]) {
+    FILE *fp = fopen(argv[1], "r");
+
+    if (!fp) {
+        perror("No file path provided");
+        return 1;
+    }
+
+    int operand;
+    int current_cell = 0;
+    int loop_start;
+
+    while ((operand = fgetc(fp)) != EOF) {
+        switch ((char) operand) {
+            case '>':
+                current_cell++;
+                break;
+            
+            case '<':
+                current_cell--;
+                break;
+
+            case '+':
+                if (turing[current_cell] == 255) {
+                    turing[current_cell] = 0;
+                } else {
+                    turing[current_cell]++;
+                }
+                break;
+            
+            case '-':
+                if (turing[current_cell] == 0) {
+                    turing[current_cell] = 255;
+                } else {
+                    turing[current_cell]--;
+                }
+                break;
+
+            case '.':
+                printf("%c", (char) turing[current_cell]);
+                break;
+
+            case ',':
+            {
+                turing[current_cell] = (unsigned int) getchar();
+                break;
+            }
+
+            case '[':
+                break;
+            
+            case ']':
+                break;
+        }
+    }
+
+    printf("\n");
+
+    return 0;
+}
